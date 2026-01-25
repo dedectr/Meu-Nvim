6cdb47169138e2deed1081a6e0b725a465b08c41
André Alencar <139161139+dedectr@users.noreply.github.com>
2026-01-25 17:57:32 +0000

    Mason update

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,58 +1,19 @@
 return {
-  -- Mason: Gerenciador de binários
   {
     "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end,
+    version = "^1",
+    build = ":MasonUpdate",
+    config = true,
   },
-
-  -- Mason LSPConfig: Ponte entre Mason e Neovim
   {
     "williamboman/mason-lspconfig.nvim",
-    dependencies = { "williamboman/mason.nvim" },
+    version = "^1",
+    dependencies = { "williamboman/mason.nvim", "neovim/nvim-lspconfig" },
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { 
-          "pyright", 
-          "html", 
-          "cssls", 
-          "vtsls", 
-          "lua_ls", 
-          "clangd" 
-        },
+        -- lista opcional; pode deixar vazio também
+        ensure_installed = { "pyright", "lua_ls", "clangd" },
       })
-    end,
-  },
-
-  -- LSP Config: Usando o novo padrão vim.lsp.config
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = { "williamboman/mason-lspconfig.nvim" },
-    config = function()
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
-      -- Servidores simples
-      local servers = { "pyright", "html", "cssls", "vtsls", "clangd" }
-      
-      for _, server in ipairs(servers) do
-        vim.lsp.config(server, {
-          capabilities = capabilities,
-        })
-        -- Ativa o servidor
-        vim.lsp.enable(server)
-      end
-
-      -- Configuração específica para Lua
-      vim.lsp.config("lua_ls", {
-        capabilities = capabilities,
-        settings = {
-          Lua = {
-            diagnostics = { globals = { "vim" } },
-          },
-        },
-      })
-      vim.lsp.enable("lua_ls")
     end,
   },
 }
